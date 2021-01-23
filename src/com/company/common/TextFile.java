07e8c84ea20087f5a68099a2b57af75dcd95924e
Piotr Malek <piotrek.malek@outlook.com>
2021-01-23 19:19:40 +0100

    Create methods to change and read file from server

--- src/com/company/common/TextFile.java
+++ src/com/company/common/TextFile.java
@@ -1,4 +1,4 @@
-package com.company.pages.common;
+package com.company.common;
 
 import java.io.Serializable;
 
--- src/com/company/pages/mainPage/MainPageFXMLController.java
+++ src/com/company/pages/mainPage/MainPageFXMLController.java
@@ -1,12 +1,14 @@
 package com.company.pages.mainPage;
 
-import com.company.pages.common.TextFile;
+import com.company.common.TextFile;
 import com.company.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.rmi.RemoteException;
@@ -17,13 +19,20 @@ public class MainPageFXMLController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        List<String> list = null;
+        List<TextFile> list = null;
         try {
             list = Main.niceNoteServer.filesList(Main.userId);
+            setFilesList(list);
+            fileContent.textProperty().addListener((observable, oldValue, newValue) -> {
+                try {
+                    Main.niceNoteServer.writeFile(Main.userId, getCurrentFileName(), newValue);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            });
         } catch (RemoteException e) {
             e.printStackTrace();
         }
-        System.out.println(list);
     }
 
     @FXML
@@ -32,8 +41,51 @@ public class MainPageFXMLController implements Initializable {
     @FXML
     private ListView<TextFile> filesList;
 
+    @FXML
+    private TextArea fileContent;
+
+    public void setFileContent(String content) {
+        fileContent.setText(content);
+    }
+
+    public void setFilesList(List<TextFile> filesList) {
+        this.filesList.getItems().addAll(filesList);
+        setCellFactory();
+    }
+
     public void addListItem(TextFile file) {
         filesList.getItems().add(file);
+        setCellFactory();
+    }
+
+    public String getCurrentFileName() {
+        return filesList.getSelectionModel().getSelectedItem().getFileName();
+    }
+
+    @FXML
+    public void changeFile(MouseEvent mouseEvent) {
+        try {
+            String fileName = getCurrentFileName();
+            String content = Main.niceNoteServer.readFile(Main.userId, fileName);
+            setFileContent(content);
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void writeFile() {
+        try {
+            System.out.println("WYWOLALO Sie");
+            String fileName = getCurrentFileName();
+            String content = fileContent.getText();
+            Main.niceNoteServer.writeFile(Main.userId, fileName, content);
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void setCellFactory() {
         filesList.setCellFactory(param -> new ListCell<TextFile>() {
             @Override
             protected void updateItem(TextFile file, boolean empty) {
